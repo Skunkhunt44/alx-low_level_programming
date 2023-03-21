e355f0ab105c5fc919fd33cb9a19db8bfbb71e49
kwame Adams <kwameadams77@gmail.com>
2023-03-21 00:23:48 +0000

    operations

--- 0x01-variable_if_else_while/0-positive_or_negative.c
+++ 0x01-variable_if_else_while/0-positive_or_negative.c
@@ -19,15 +19,17 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-	printf("is positive")
+	printf("%d is positive," n)
 	}
 	else if (n == 0)
 	{
-	printf("is zero")
+	printf("%d is zero," n)
 	}
 	else if (n > 0)
 	{
-	printf("is negative")
+	printf("%d is negative," n)
 	}
+	printf("\n");
 	return (0);
+
 }
