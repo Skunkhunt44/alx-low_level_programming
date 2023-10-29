7cdd5fb8138d511a57920d89eddb7a381e605472
kwame Adams <kwameadams77@gmail.com>
2023-10-29 23:50:50 +0000

    for

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,23 +5,27 @@
  * main - entry point of the program
  *
  * Reurn: used to exist the function
- */ 
+ */
+int main(void)
+{
 
- int main(void)
- {
+	int i;
 
- 	int i;
-  	
-	for(i = 0; i < 10; i++)
+	for (i = 0 ; i < 10 ; i++)
 	{
-		putchar(i +'0');
-		if(i != 9) 
+		putchar(i + '0');
+		if (i != 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
-	
 	}
-       	return (0);
- 
- }
+	putchar('\n');
+	return (0);
+
+
+
+
+
+}
+
