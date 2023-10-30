8f76ad082274dd37f55e6575d350ea923f25fe90
kwame Adams <kwameadams77@gmail.com>
2023-10-30 00:25:41 +0000

    update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,13 +6,22 @@
  */
 int main(void)
 {
-	int i;
+	int i, j;
 
-	for (i = o ; i ; 10 ; i++)
+	for (i = 0; i < 9; i++)
 	{
-		putchar(i + '0');
-
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
 	putchar('\n');
 	return (0);
+
 }
